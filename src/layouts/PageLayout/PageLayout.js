f9ae35afb0e4fc6fc7ed970489939a79a0491032
Tyler Barabas <tylerb@d2.com>
2017-06-25 14:11:59 -0700

    first commit

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import { IndexLink, Link } from 'react-router'
+//import { IndexLink, Link } from 'react-router'
 import PropTypes from 'prop-types'
-import './PageLayout.scss'
 
 export const PageLayout = ({ children }) => (
   <div className='container text-center'>
-    <h1>React Redux Starter Kit</h1>
-    <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>
+    <p>Make your acme codes here</p>
     <div className='page-layout__viewport'>
       {children}
     </div>
