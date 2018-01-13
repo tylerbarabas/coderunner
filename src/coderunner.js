4b459a093352dd09761aa17343471fb0c932e2ed
Tyler Barabas <tylerbarabas@gmail.com>
2018-01-12 19:33:57 -0800

    its now making requests for the new image and for progress

--- src/coderunner.js
+++ src/coderunner.js
@@ -1,9 +1,13 @@
-import { getAnimsJson } from './service';
+import { getAnimsJson, newOrder, checkProgress } from './service';
 
 export default class Coderunner {
     constructor(){
         this.animations = null;
+        this.orderNumber = null;
+        this.progress = null;
+
         this.setScreenOrientation = this.setScreenOrientation.bind(this);
+        this.scanDestinationChanged = this.scanDestinationChanged.bind(this);
     }
 
     init(){
@@ -14,6 +18,7 @@ export default class Coderunner {
 
     enableListeners(){
         window.addEventListener('resize', this.setScreenOrientation);
+        document.getElementById('scan-destination').addEventListener('change', this.scanDestinationChanged);
     }
 
     getAnimations(){    
@@ -30,5 +35,34 @@ export default class Coderunner {
             c.className = c.className.split(' portrait')[0].split(' landscape')[0];
             c.className += ' ' + orientation;
         }
+    }
+
+    scanDestinationChanged( e ) {
+        newOrder({ msg: e.target.value }).then( res => {
+            this.orderNumber = res.orderNumber;
+            if ( this.progressLoop !== null ) this.stopProgressLoop();
+            this.startProgressLoop();
+        });
+    }
+
+    startProgressLoop(){
+        this.progressLoop = window.setInterval(() => {
+            if (this.progress < 100) {
+                this.getProgress();
+            } else {
+                this.stopProgressLoop();
+            }
+        }, 1000);
+    }
+
+    stopProgressLoop(){
+        window.clearInterval( this.progressLoop );
+        this.progressLoop = null;
+    }
+
+    getProgress( orderNumber = this.orderNumber ) {
+        checkProgress( orderNumber ).then( json => {
+            this.progress = json.progress;
+        });
     }
 }
--- src/service.js
+++ src/service.js
@@ -4,3 +4,13 @@ export async function getAnimsJson(){
     let res = await fetch( domain + '/anims-json');
     return await res.json();
 }
+
+export async function newOrder( params ){
+    let res = await fetch( domain + '/new?msg=' + params.msg );
+    return await res.json();
+}
+
+export async function checkProgress( orderNumber ){
+    let res = await fetch( domain + '/orders/' + orderNumber + '/progress');
+    return await res.json();
+}
